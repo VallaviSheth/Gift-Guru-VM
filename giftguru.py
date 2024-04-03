4f7043cad41d87152148b83dcc5ae357b14ab001
Haadi Rakhangi <127193364+haadirakhangi@users.noreply.github.com>
2024-04-03 10:44:00 +0530

    mehek is here

--- giftguru.py
+++ giftguru.py
@@ -4,65 +4,64 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain.output_parsers import StructuredOutputParser, ResponseSchema, PydanticOutputParser
 from langchain_core.pydantic_v1 import BaseModel, Field
-from dotenv import load_dotenv,find_dotenv
+from dotenv import load_dotenv, find_dotenv
 import openai
 from typing import List
 import os
 from serpapi import GoogleSearch
+from PIL import Image
+import io
 
 load_dotenv(find_dotenv())
 openai.api_key = os.environ['OPENAI_API_KEY']
 
-
 class GiftGuru(BaseModel):
-    gift : List[str] = Field(description="The list of gifts to recommend based on the scenario.")
-    reason: List[str] = Field(description = "Corresponding list of reasons to recommend the gift to the user. Should be atleast 20 words each")
+    gift: List[str] = Field(description="The list of gifts to recommend based on the scenario.")
+    reason: List[str] = Field(description="Corresponding list of reasons to recommend the gift to the user. Should be at least 20 words each")
 
 def gift_recommendation(query):
-    model = ChatOpenAI(model = 'gpt-3.5-turbo-1106', temperature =0.0)
+    model = ChatOpenAI(model='gpt-3.5-turbo-1106', temperature=0.0)
     template = """You will be given a scenario from the user and you have to suggest \
     the best gifts as well as the reason for recommending the gift. There should be gifts which should be something that might be available on amazon \
     considering all the scenarios. The output should be a json with the following instructions:\n{format_instructions}
 
     Scenario: {scenario}"""
-    pydantic_output_parser = PydanticOutputParser(pydantic_object = GiftGuru)
+    pydantic_output_parser = PydanticOutputParser(pydantic_object=GiftGuru)
     prompt = ChatPromptTemplate.from_template(template)
     format_instructions = pydantic_output_parser.get_format_instructions()
-    final_prompt = prompt.partial(format_instructions = format_instructions)
+    final_prompt = prompt.partial(format_instructions=format_instructions)
     chain = final_prompt | model | pydantic_output_parser
     response = chain.invoke({"scenario": query})
 
     return response
 
-
-# Function to display the gift and reason as cards
 def display_gift_cards(response):
     for i in range(len(response.gift)):
         st.write("## Gift:", response.gift[i])
         st.write("### Reason:", response.reason[i])
-        
-        if st.button(f"Get Items",key=response.gift[i]):
+
+        if st.button(f"Get Items", key=response.gift[i]):
             get_amazon_links(response.gift[i])
 
-
 def get_amazon_links(query):
-    print("calling links in amazon")
-    query = query +"-amazon"
+    print("Fetching links from amazon")
+    query = query + "-amazon"
     params = {
-    "engine": "google",
-    "q": query,
-    "api_key": os.environ['GOOGLE_SERP_API_KEY']
+        "engine": "google",
+        "q": query,
+        "api_key": os.environ['GOOGLE_SERP_API_KEY']
     }
     search = GoogleSearch(params)
     results = search.get_dict()
     organic_results = results["organic_results"]
-    print("google response",organic_results)
     for result in organic_results:
         st.image(result['thumbnail'])
         st.write(f" {result['title']}")
         st.markdown(f"[{result['link']}]({result['link']})")
 
-
+def image_input(uploaded_file):
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
 
 def main():
     st.title("GiftGuru")
@@ -74,6 +73,12 @@ def main():
     if user_prompt:
         # Display recommended gifts and reasons
         display_gift_cards(res)
+
+    # Image uploader
+    uploaded_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+    if uploaded_file is not None:
+        image_input(uploaded_file)
+        
 
 if __name__ == "__main__":
     main()
